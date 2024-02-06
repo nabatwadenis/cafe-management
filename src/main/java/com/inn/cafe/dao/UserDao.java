2108ca722eabc38f3132981913ee58a309df16ad
nabatwadenis <dnabatwa9@gmail>
2024-02-06 13:12:32 +0300

    generate auth token

--- src/main/java/com/inn/cafe/dao/UserDao.java
+++ src/main/java/com/inn/cafe/dao/UserDao.java
@@ -4,7 +4,7 @@ import com.inn.cafe.POJO.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface UserDao extends JpaRepository<User, Integer> {
+public interface  UserDao extends JpaRepository<User, Integer> {
 
     User findByEmail(@Param("email")String email);
 }
